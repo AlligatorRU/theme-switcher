d1c6cdce38868be39f6ba9315ce5ff39f75b7bbe
Oleg Gruzdev <Gr.O.S@ya.ru>
2025-01-07 16:37:28 +0300

    update

---
+++ theme-switcher-day-night.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the themes
+DAY_THEME="BreezeClassic"
+NIGHT_THEME="BreezeDark"
+WALLPAPERS_DAY="/home/alligator/Изображения/wallpapers/Holmy.jpg"
+WALLPAPERS_NIGHT="/home/alligator/Изображения/wallpapers/Holmy_Night.jpg"
+COLOR_DAY="#f1c992"
+COLOR_NIGHT="#92aef1"
+
+# Get the current theme marked as "current"
+CURRENT_THEME=$(plasma-apply-colorscheme -l | grep "текущая цветовая схема" | awk '{print $2}')
+
+# Check if the desired theme is already active
+if [[ "$CURRENT_THEME" == "$DAY_THEME" ]]; then
+    plasma-apply-colorscheme "$NIGHT_THEME"
+    plasma-apply-wallpaperimage $WALLPAPERS_NIGHT
+    plasma-apply-colorscheme -a "$COLOR_NIGHT"
+    plasma-apply-colorscheme -a "$COLOR_NIGHT"
+    notify-send -a theme-switcher $NIGHT_THEME "успешно задана для использования в Plasma"
+    paplay /home/$USER/.theme-switcher/sound.wav
+    else
+    plasma-apply-colorscheme "$DAY_THEME"
+    plasma-apply-wallpaperimage $WALLPAPERS_DAY
+    plasma-apply-colorscheme -a "$COLOR_DAY"
+    plasma-apply-colorscheme -a "$COLOR_DAY"
+    notify-send -a theme-switcher $DAY_THEME "успешно задана для использования в Plasma"
+    paplay /home/$USER/.theme-switcher/sound.wav
+fi
+
